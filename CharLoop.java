ab7a97b1e39f29ddffce3b028de44558300ff66b
Narsing Tudme <118840057+narsingtudme@users.noreply.github.com>
2024-03-18 19:39:09 +0530

    Add files via upload

---
+++ CharLoop.java
@@ -0,0 +1,25 @@
+class CharLoop
+{
+	public static void main(String[] args) {
+		// System.out.println("*****");
+		// for (char ch=97;ch<='z' ;ch++ ) {
+		// 	System.out.println(ch+" ");
+		// }
+		// System.out.println("*****");
+		// for (char ch='a';ch<=122 ;ch++ ) {
+		// 	System.out.println(ch+" ");
+		// }
+		// System.out.println("*****");
+		// for (char ch=67;ch<='Z' ;ch++ ) {
+		// 	System.out.println(ch+" ");
+		// }
+		// System.out.println("*****");
+		// for (char ch='A';ch<=90 ;ch++ ) {
+		// 	System.out.println(ch+" ");
+		// }
+		System.out.println("*****");
+		for (char ch=90;ch>='A' ;ch-- ) {
+			System.out.println(ch+" ");
+		}
+	}
+}
